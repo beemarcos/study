2fbf4c83aeec435e91d1c47a070e7508fcd7c270
Marcos Bee <beemarcos72@gmail.com>
2023-02-13 23:15:26 -0300

    First Commit

---
+++ helloworld.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    int vetor [10];
+
+
+    vetor[0]=0;
+    vetor[1]=5;
+    vetor[2]=2;
+    vetor[3]=1;
+    vetor[4]=3;
+    vetor[5]=4;
+    vetor[6]=55;
+    vetor[7]=77;
+    vetor[8]=99;
+    vetor[9]=45;
+    printf("Início do programa\n");
+    
+    for(int i=0;i<10;i++){
+        printf("Vetor [%d]: %d \n" , i, vetor[i]);
+    }
+
+    /*
+    for(int i=0;i<10;i++){
+        printf("Vetor na posição %d:",i);
+        scanf("%d", &vetor[i]);
+    }
+    */
+s
+
+
+
+
+
+
+
+
+    for(int i=0;i<10;i++){
+        printf("Vetor [%d]: %d \n" , i, vetor[i]);
+    }
+    printf("Fim do programa\n");
+    
+
+}
