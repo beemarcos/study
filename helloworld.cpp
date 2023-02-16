c584a742a2cf1c57263058f1904a7a5ebca6e62e
Marcos Bee <beemarcos72@gmail.com>
2023-02-15 23:23:13 -0300

    Insert Sort do livro do Cormen

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,50 +1,83 @@
+//Exemplo de Insert sort do livro do Cormen pg12
+
 #include <iostream>
 #include <vector>
 #include <string>
 
 using namespace std;
 
+int lenght;
+
+int InserionSort (int vetor[]){
+    
+    int chave;
+    int i;
+
+    printf("Running...");
+    for( int j=1 ; j<lenght ; j++){
+        chave = vetor[j];
+        i=j-1;
+        while (i>-1 && vetor[i]>chave)
+        {
+            vetor[i+1] = vetor[i];
+            i=i-1;
+        }
+        vetor[i+1]=chave;
+        printf("x, ");        
+    }
+
+    for(int i=0;i<10;i++){
+        printf("\nVetor depois(dentro) [%d]: %d " , i, vetor[i]);
+    }
+
+    return 0;
+}
+
+
+
 int main()
 {
-
+    int f;
     int vetor [10];
+    
 
-
-    vetor[0]=0;
+    vetor[0]=71;
     vetor[1]=5;
-    vetor[2]=2;
+    vetor[2]=53;
     vetor[3]=1;
-    vetor[4]=3;
+    vetor[4]=69;
     vetor[5]=4;
     vetor[6]=55;
-    vetor[7]=77;
+    vetor[7]=2;
     vetor[8]=99;
     vetor[9]=45;
     printf("Início do programa\n");
     
     for(int i=0;i<10;i++){
-        printf("Vetor [%d]: %d \n" , i, vetor[i]);
+        printf("\nVetor antes [%d]: %d " , i, vetor[i]);
     }
 
-    /*
+
+    /* 
+    EXEMPLO DE SCANF
     for(int i=0;i<10;i++){
         printf("Vetor na posição %d:",i);
         scanf("%d", &vetor[i]);
     }
     */
-s
 
+ 
 
+    printf("\nTamanho %d ", (int)sizeof(vetor)/(int)sizeof(vetor[0]));
+    lenght=(int)sizeof(vetor)/(int)sizeof(vetor[0]);
 
-
-
-
+    f=InserionSort(&vetor[0]);
 
 
     for(int i=0;i<10;i++){
-        printf("Vetor [%d]: %d \n" , i, vetor[i]);
+        printf("\nVetor depois [%d]: %d " , i, vetor[i]);
     }
-    printf("Fim do programa\n");
+    printf("\nFim do programa");
     
 
 }
